7c19bf239776b45729180e6c19ad2cdf58406fa6
gpapadok <38889721+gpapadok@users.noreply.github.com>
2018-05-04 04:12:57 +0300

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,35 @@
+
+
+class Board:
+	wPieces = ['wR','wN','wB','wQ','wK','wB','wN','wR']
+	wPieces += 8 * ['wP']
+	bPieces = ['bR','bN','bB','bQ','bK','bB','bN','bR']
+	bPieces += 8 * ['bP']
+
+	board = [[0 for x in range(8)] for y in range(8)]
+
+	def __init__(self):
+		self.board[0] = self.bPieces[:8]
+		self.board[1] = self.bPieces[9:]
+		self.board[6] = self.wPieces[9:]
+		self.board[7] = self.wPieces[:8]
+
+
+	def print(self):
+		for j in range(8):
+			print(self.board[j])
+		
+
+
+
+def playchess():
+	board = Board()
+	board.print()
+
+x = 1
+if x == \
+1:
+    print('hello')
+#playchess()
+
+
